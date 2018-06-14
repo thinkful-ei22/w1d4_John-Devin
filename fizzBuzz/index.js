125b0e1c27cf657ffc013a3545e40812d55f911e
dghero <devingreyhero@gmail.com>
2018-06-14 14:07:10 -0500

    Partially complete fizzbuzz

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -1,24 +1,43 @@
+'use strict';
+
 const submitHandler = function(e){
-	$("#number-chooser").submit(function() {
-		event.preventDefault();
-		const choice = $("#number-choice").val();
-		console.log(choice);
-	});
+  $('#number-chooser').submit(function() {
+    event.preventDefault();
+    const choice = $('#number-choice').val();
+    console.log(choice);
+		
+    for(let i = 1; i <= choice; i++){
+      console.log(fizzBuzz(i));
+			$('.js-results').append(fizzBuzzHtml(i));
+    }
+
+  });
+};
+
+function fizzBuzzHtml(n){
+	let fizzy = fizzBuzz(n);
+	let fizzBuzzClass = '';
+	
+	if(typeof fizzy === 'string'){
+		fizzBuzzClass = fizzBuzz;
+	}
+
+	return `<div class="fizz-buzz-item ${fizzBuzzClass}"> <span>${fizzy}</span> </div>`;
 }
 
 submitHandler();
 
 
 
-// const fizzBuzz = function(num){
+function fizzBuzz (num){
 
-//   let result = '';
-//   if(num%3 !== 0 && num%5 !== 0)
-//     return num;
-//   if(num%3 === 0)
-//     result += 'fizz';
-//   if(num%5 === 0)
-//     result += 'buzz';
+  let result = '';
+  if(num%3 !== 0 && num%5 !== 0)
+    return num;
+  if(num%3 === 0)
+    result += 'fizz';
+  if(num%5 === 0)
+    result += 'buzz';
 
-//   return result;
-// };
+  return result;
+}
