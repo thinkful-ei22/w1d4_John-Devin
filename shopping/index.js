4c91e665a14e4d81d3944f8199003248c56d810a
dghero <devingreyhero@gmail.com>
2018-06-14 15:20:18 -0500

    Begin deleteHandler

--- shopping/index.js
+++ shopping/index.js
@@ -1,1 +1,23 @@
+'use strict';
 
+//Handle add
+const addHandler = function(e){
+  $('.js-shopping-list-form').submit(function(e) {
+    
+  });
+};
+
+
+
+
+//Handle delete
+const deleteHandler = function(e){
+  $('.shopping-item').on('click', 'shopping-item-delete', function(e){
+    console.log('Delete button clicked, no bubble!');
+  });
+};
+
+
+deleteHandler();
+
+//Handle check
