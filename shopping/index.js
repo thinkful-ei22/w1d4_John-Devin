f5d3230a37f3aefef2cc5e9c113c2c898c4292c6
dghero <devingreyhero@gmail.com>
2018-06-14 15:39:28 -0500

    Complete Delete functionality for shopping

--- shopping/index.js
+++ shopping/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log('Script linked');
+
 //Handle add
 const addHandler = function(e){
   $('.js-shopping-list-form').submit(function(e) {
@@ -8,16 +10,16 @@ const addHandler = function(e){
 };
 
 
-
-
 //Handle delete
 const deleteHandler = function(e){
-  $('.shopping-item').on('click', 'shopping-item-delete', function(e){
-    console.log('Delete button clicked, no bubble!');
+  $('.shopping-list').on('click', '.shopping-item-delete', function(e){
+    //console.log('Delete button clicked!');
+    $(e.currentTarget).closest('li').remove();
   });
 };
 
-
-deleteHandler();
+$(function() { 
+  deleteHandler();
+});
 
 //Handle check
