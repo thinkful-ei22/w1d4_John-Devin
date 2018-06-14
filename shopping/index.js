9d461202677320bc0ea4c4bf4e4daeda78cf2b88
dghero <devingreyhero@gmail.com>
2018-06-14 16:25:51 -0500

    Complete add functionality to shopping

--- shopping/index.js
+++ shopping/index.js
@@ -4,11 +4,31 @@ console.log('Script linked');
 
 //Handle add
 const addHandler = function(e){
-  $('.js-shopping-list-form').submit(function(e) {
-    
+  $('#js-shopping-list-form').submit(function() {
+    event.preventDefault();
+    //fetch item name
+    const itemName = $('.js-shopping-list-entry').val();
+
+    //append item to ul
+    if(itemName.length > 0)
+      appendItem(itemName);
   });
 };
 
+const appendItem = function(name){
+  const newItem = '<li>'
+        + `<span class="shopping-item">${name}</span>`
+        + '<div class="shopping-item-controls">'
+        +  '<button class="shopping-item-toggle">'
+        +    '<span class="button-label">check</span>'
+        +  '</button>'
+        +  '<button class="shopping-item-delete">'
+        +    '<span class="button-label">delete</span>'
+        +  '</button>'
+        +'</div>'
+        +'</li>';
+  $('.shopping-list').append(newItem);
+};
 
 //Handle delete
 const deleteHandler = function(e){
@@ -24,6 +44,7 @@ const strikeThroughHandler = function(e){
 };
 
 $(function() { 
+  addHandler();
   deleteHandler();
   strikeThroughHandler();
 });
