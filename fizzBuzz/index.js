409a8b1632557ae41d502edd8b8be1b55d301bf0
dghero <devingreyhero@gmail.com>
2018-06-14 14:24:01 -0500

    Complete fizzbuzz

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -1,34 +1,5 @@
 'use strict';
 
-const submitHandler = function(e){
-  $('#number-chooser').submit(function() {
-    event.preventDefault();
-    const choice = $('#number-choice').val();
-    console.log(choice);
-		
-    for(let i = 1; i <= choice; i++){
-      console.log(fizzBuzz(i));
-			$('.js-results').append(fizzBuzzHtml(i));
-    }
-
-  });
-};
-
-function fizzBuzzHtml(n){
-	let fizzy = fizzBuzz(n);
-	let fizzBuzzClass = '';
-	
-	if(typeof fizzy === 'string'){
-		fizzBuzzClass = fizzBuzz;
-	}
-
-	return `<div class="fizz-buzz-item ${fizzBuzzClass}"> <span>${fizzy}</span> </div>`;
-}
-
-submitHandler();
-
-
-
 function fizzBuzz (num){
 
   let result = '';
@@ -38,6 +9,34 @@ function fizzBuzz (num){
     result += 'fizz';
   if(num%5 === 0)
     result += 'buzz';
-
   return result;
 }
+
+const submitHandler = function(e){
+  $('#number-chooser').submit(function() {
+    event.preventDefault();
+    const choice = $('#number-choice').val();
+		
+    $('.js-results').empty();
+
+    for(let i = 1; i <= choice; i++){
+      $('.js-results').append(fizzBuzzHtml(i));
+    }
+
+  });
+};
+
+function fizzBuzzHtml(n){
+  let fizzy = fizzBuzz(n);
+  let fizzBuzzClass = '';
+	
+  if(typeof fizzy === 'string'){
+    fizzBuzzClass = fizzy;
+  }
+
+  return `<div class="fizz-buzz-item ${fizzBuzzClass}"> <span>${fizzy}</span> </div>`;
+}
+
+submitHandler();
+
+
