ffd75b093c28617df0be96bfd5a5d4700dbf4d07
dghero <devingreyhero@gmail.com>
2018-06-14 13:24:55 -0500

    Complete Cat Carousel

--- catcarousel/index.js
+++ catcarousel/index.js
@@ -1,23 +1,9 @@
 'use strict';
 
-
 function handleImage() {
   $('.thumbnail').on('click', event => {
-    let targetImgSrc;
-    let targetImgAlt;
-    
-    console.log($(event.target));
-    
-    if(event.which === 13){
-      console.log('Hello!');
-      targetImgSrc = $(event.target).last().attr('src');
-      targetImgAlt = $(event.target).last().attr('alt');
-    }else{
-      targetImgSrc = $(event.target).attr('src');
-      targetImgAlt = $(event.target).attr('alt');
-    }
-    
-    console.log(targetImgSrc);
+    const targetImgSrc = $(event.currentTarget).find('img').attr('src');
+    const targetImgAlt = $(event.currentTarget).find('img').attr('alt');
 
     console.log($('.hero > img').attr('src', targetImgSrc));
     console.log($('.hero > img').attr('alt', targetImgAlt));
